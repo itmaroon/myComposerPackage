1a713a13514fc765664f4df707358ad1590356f7
itmaroon <master@itmaroon.net>
2023-12-26 20:30:08 +0900

    second commit

--- src/MyCommandClass.php
+++ src/MyCommandClass.php
@@ -5,7 +5,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
     class MyCommandClass extends \WP_CLI_Command {
 		public function __invoke( $args, $assoc_args ) {
 			list( $category ) = $args;
-			\WP_CLI::line("Command args: {$category}");
+			\WP_CLI::line("Congraturation: {$category}");
 		}
 	}
 }
